65e62282231526319e33bc89a1aaba1757d4f97a
narvas12 <ebukaemmanuel71@gmail.com>
2023-01-28 10:02:33 +0100

    buid

--- project/settings.py
+++ project/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-3$iqvpem2pdl@01g5o5lmh6^0z56i!1#d6vs87h)cpg@wdg2+y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -124,6 +124,9 @@ if DEBUG:
 ]
 else:
     STATIC_ROOT = BASE_DIR / "static"
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
