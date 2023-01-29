a388ff48ad2a8ade6dafc9c54471a7948a3c8496
narvas12 <ebukaemmanuel71@gmail.com>
2023-01-29 09:56:50 +0100

    postgreSQL

--- project/settings.py
+++ project/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'pradech360',
+        'PASSWORD': '<Script.!@#.123>',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
