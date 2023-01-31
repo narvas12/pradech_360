5efec56700b85f785aa5f382e85105c84daa8a7b
narvas12 <ebukaemmanuel71@gmail.com>
2023-01-31 14:14:49 +0100

    Deployment issue

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,13 @@
-pip install -r requirements.txt
-# python manage.py collectstatic
+#!/bin/bash
+
+#build project 
+echo "Building Priject..."
+python -m install -r requirements.txt
+
+echo "Make Migrations" 
+
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+echo "collect static"
+python manage.py collectstatic --noinput --clear
